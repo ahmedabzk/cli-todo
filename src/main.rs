b4cf2f1dbe0cb6b98eef7da7623ad1ef13c5e846
ahmedabzk <ahmed.abzk@gmail.com>
2023-10-04 21:40:54 +0300

    cli todo app

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+
+use todo_app::Todo;
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: todo_app <action> [item]");
+        return;
+    }
+
+    let action = &args[1];
+
+    let mut todo = Todo::new().expect("Initialisation of db failed");
+
+    if action == "add"{
+        if args.len() < 3 {
+            println!("Please provide an item to add.");
+            return;
+        }
+        let item = &args[2];
+
+        todo.insert(item.to_string());
+        match todo.save(){
+            Ok(_) => println!("Todo Saved"),
+            Err(e) => println!("error occurred: {}", e)
+        }
+    }else if action =="complete"{
+        if args.len() < 3 {
+            println!("Please provide an item to mark as complete.");
+            return;
+        }
+        let item = &args[2];
+        match todo.complete(&item){
+            None => println!("{} is not present", item),
+            Some(_) => match todo.save(){
+                Ok(_) => println!("todo saved"),
+                Err(e) => println!("an error occurred: {}",e)
+            }
+        }
+    }else if action =="list"{
+        for (i,v) in todo.list().iter().enumerate(){
+            println!("{}:{}",i+1, v)
+        }
+    }
+}
